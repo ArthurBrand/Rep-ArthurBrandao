7a682a4ebedf051d6ca09636fb3eb71a9bd4fe08
ArthurBrand <87667667+ArthurBrand@users.noreply.github.com>
2022-08-04 10:15:28 -0300

    Add files via upload

---
+++ script consulta vm por csv.ps1
@@ -0,0 +1,12 @@
+﻿Install-Module -Name PowerShellGet -Force
+
+Connect-AzAccount
+
+Import-Csv -Path 'caminhodoarquivo.csv' -Encoding UTF8 -Delimiter ";" | ForEach-Object {
+    
+    $nome = $_.nomeVm
+    
+
+    Get-AzVM -status -Name $nome | Select Name, PowerState
+}
+
