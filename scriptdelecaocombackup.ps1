912479d57bf53e073a72aed9fd35bab7f23700c6
ArthurBrand <87667667+ArthurBrand@users.noreply.github.com>
2022-08-04 10:14:08 -0300

    Add files via upload

---
+++ scriptdelecaocombackup.ps1
@@ -0,0 +1,25 @@
+﻿#Connect-ExchangeOnline
+#Connect-MsolService
+
+#Coletar lista
+Import-CSV "C:\temp\lista\testedistrib.csv" -Encoding UTF8 -Delimiter ";" | ForEach-Object {
+    
+    $listdistrib = $_.Nomelista
+    
+    write-host "Exportando usuário da lista "$listdistrib
+
+    Get-DistributionGroupMember -Identity "$listdistrib" | Select-Object PrimarySmtpAddress | export-csv C:\lista\$(($listdistrib).ToString()).csv
+	
+	#Remove-DistributionGroup -Identity "$listdistrib"
+}
+
+
+
+#Exportar usuario de cada lista
+#Get-DistributionGroupMember -Identity "$lista" | Select-Object PrimarySmtpAddress | export-csv C:\temp\lista\$(($lista).ToString()).csv
+
+#Deleção
+
+#Remove-DistributionGroup -Identity "Temporary Staff"
+
+
